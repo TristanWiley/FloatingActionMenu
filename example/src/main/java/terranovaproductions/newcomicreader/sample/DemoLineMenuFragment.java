9ccf25b99919d4590b82e4a3e19d1dd0b0305a3b
Tristan Wiley <trwiley@twcny.rr.com>
2015-11-05 13:44:38 -0500

    Changed package

--- example/src/main/java/terranovaproductions/newcomicreader/sample/DemoLineMenuFragment.java
+++ example/src/main/java/terranovaproductions/newcomicreader/sample/DemoLineMenuFragment.java
@@ -1,4 +1,4 @@
-package terranovaproductions.newcomicreader.sample;
+package novaapps.floatingactionmenu.sample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import terranovaproductions.newcomicreader.FloatingActionMenu;
+import novaapps.floatingactionmenu.FloatingActionMenu;
 
 /**
  * Created by renqinghe on 15-11-4.
