b15d41acded558bfbb8f043587af8e67070068e3
Tristan Wiley <trwiley@twcny.rr.com>
2015-11-05 13:46:36 -0500

    Changed package

--- example/src/main/java/terranovaproductions/newcomicreader/sample/FragmentAdapter.java
+++ example/src/main/java/terranovaproductions/newcomicreader/sample/FragmentAdapter.java
@@ -1,4 +1,4 @@
-package terranovaproductions.newcomicreader.sample;
+package novaapps.floatingactionmenu.sample;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
