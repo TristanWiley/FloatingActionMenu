430f116c1a5f9df168f87d22875cea43a6063b68
Tristan Wiley <trwiley@twcny.rr.com>
2015-11-05 14:06:54 -0500

    Update DemoCircleMenuFragment.java

--- example/src/main/java/terranovaproductions/newcomicreader/sample/DemoCircleMenuFragment.java
+++ example/src/main/java/terranovaproductions/newcomicreader/sample/DemoCircleMenuFragment.java
@@ -1,4 +1,4 @@
-package novaapps.floatingactionmenu.sample;
+package terranovaproductions.newcomicreader.sample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import novaapps.floatingactionmenu.FloatingActionMenu;
+import terranovaproductions.newcomicreader.FloatingActionMenu;
 
 /**
  * Created by renqinghe on 15-11-4.
